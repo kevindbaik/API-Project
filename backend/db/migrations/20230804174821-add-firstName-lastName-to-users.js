9dbc2c2026ad0853468a7407a62ad170c59f0bb3
Kevin <kevindbaik@gmail.com>
2023-08-04 12:58:05 -0700

    finished p5 1.0

--- backend/db/migrations/20230804174821-add-firstName-lastName-to-users.js
+++ backend/db/migrations/20230804174821-add-firstName-lastName-to-users.js
@@ -5,8 +5,9 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-options.tableName = "Users";
 /** @type {import('sequelize-cli').Migration} */
+options.tableName = "Users";
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn(options, 'firstName', {
