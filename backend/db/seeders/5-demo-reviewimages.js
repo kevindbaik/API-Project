2178015f81ac00a168cc0412e22eeff8f3b029ad
Kevin <kevindbaik@gmail.com>
2023-08-07 14:34:20 -0700

    finished seeding

---
+++ backend/db/seeders/5-demo-reviewimages.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { ReviewImage } = require('../models')
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const validReviewImages = [
+  {
+    reviewId: 1,
+    url: "https://airbnb.com/blonded/uploads/9942"
+  },
+  {
+    reviewId: 2,
+    url: "https://airbnb.com/michelleobama/uploads/1343"
+  },
+  {
+    reviewId: 3,
+    url: "https://airbnb.com/boygenius/uploads/4245"
+  }
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    try {
+      await ReviewImage.bulkCreate(validReviewImages, {validate: true})
+    } catch(err) {
+      console.log(err);
+      throw err
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    await queryInterface.bulkDelete(options)
+  }
+};
---
+++ backend/db/seeders/6-demo-spotimages.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { SpotImage } = require('../models')
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+
+const validSpotImages = [
+  {
+    spotId: 1,
+    url: "https://icelandmansion.com",
+    preview: true
+  },
+  {
+    spotId: 2,
+    url: "https://chinohillsapartment.com",
+    preview: true
+  },
+  {
+    spotId: 3,
+    url: "https://seattlepikecondos.com",
+    preview: false
+  }
+]
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    try {
+      await SpotImage.bulkCreate(validSpotImages, {validate: true})
+    } catch(err) {
+      console.log(err);
+      throw err
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
+    await queryInterface.bulkDelete(options)
+  }
+};
