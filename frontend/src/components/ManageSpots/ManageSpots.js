c09fe878caa7048a08be0fa30f85c9fe4b2b3977
Kevin <kevindbaik@gmail.com>
2023-09-03 16:43:04 -0700

    styled additional media query, ready for grading

--- frontend/src/components/ManageSpots/ManageSpots.js
+++ frontend/src/components/ManageSpots/ManageSpots.js
@@ -1,48 +1,53 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { thunkLoadUserSpots } from '../../store/spots';
-import OneSpot from '../OneSpot/OneSpot';
-import '../AllSpots/AllSpots.css';
-import './ManageSpots.css'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { thunkLoadUserSpots } from "../../store/spots";
+import OneSpot from "../OneSpot/OneSpot";
+import "../AllSpots/AllSpots.css";
+import "./ManageSpots.css";
 
 function ManageSpots() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const user = useSelector(state => state.session.user)
-  const objUserSpots = useSelector(state => state.spots.allSpots);
+  const user = useSelector((state) => state.session.user);
+  const objUserSpots = useSelector((state) => state.spots.allSpots);
   const arrUserSpots = Object.values(objUserSpots);
   let noSpots = false;
 
   useEffect(() => {
-    dispatch(thunkLoadUserSpots())
+    dispatch(thunkLoadUserSpots());
   }, [dispatch]);
 
-  if(!user) history.push('/');
+  if (!user) history.push("/");
 
-  if(Object.keys(objUserSpots).length === 0) {
+  if (Object.keys(objUserSpots).length === 0) {
     noSpots = true;
-  };
+  }
 
- const handleCreate = () => {
-    const path = '/spots/new';
+  const handleCreate = () => {
+    const path = "/spots/new";
     history.push(path);
   };
 
   return (
-    <div className='managespotspage-container'>
-      <div className='manage-header'>
-      <h1>Manage Your Spots</h1>
-      {noSpots && <button className='manage-createbutton' onClick={handleCreate}>Create a New Spot</button>}
+    <div className="managespotspage-container">
+      <div className="manage-header">
+        <h1>Manage Your Spots</h1>
+        {noSpots && (
+          <button className="manage-createbutton" onClick={handleCreate}>
+            Create a New Spot
+          </button>
+        )}
+      <div className="allspots-container managespots-container">
+        {arrUserSpots.map((spot) => (
+          <OneSpot spot={spot} />
+        ))}
+      </div>
       </div>
 
-      <div className='allspots-container managespots-container'>
-        {arrUserSpots.map(spot => (<OneSpot spot={spot}/>))}
-      </div>
     </div>
-
-  )
+  );
 }
 
-export default ManageSpots
+export default ManageSpots;
--- frontend/src/components/SpotEditForm/SpotEditForm.js
+++ frontend/src/components/SpotEditForm/SpotEditForm.js
@@ -58,10 +58,8 @@ function SpotEditForm() {
     if (state.length < 1) errorsObj["state"] = "State is required.";
     if (country.length < 2) errorsObj["country"] = "Country is required.";
     if (name.length < 1) errorsObj["name"] = "Name is required.";
-    if (description.length < 30)
-    if(description.length > 250)
-    errorsObj['description'] = 'Description cannot exceed 250 characters.'
-      errorsObj["description"] = "Description needs 30 or more characters.";
+    if (description.length < 30) errorsObj["description"] = "Description needs 30 or more characters.";
+    if(description.length > 250) errorsObj['description'] = 'Description cannot exceed 250 characters.'
     if (price <= 0) errorsObj["price"] = "Price per night is required.";
     if (preview.length < 1) errorsObj["previewLength"] = "Preview image is required.";
     if(
